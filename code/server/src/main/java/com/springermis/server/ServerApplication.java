06fb8cd8a1f3b6885a84a35b76710ef1e6794f1f
Antonios Kaparounakis <antonioskaparounakis@outlook.com>
2024-02-23 08:00:39 +0200

    Initial Commit 🔥

---
+++ code/server/src/main/java/com/springermis/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.springermis.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
